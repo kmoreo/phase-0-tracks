91b5b9e96dc8ad0f53cf4119529202f5e4d3d258
Kristen Moore <kristen@codefu.org>
2016-11-20 14:44:03 -0600

    Initial commit with basic questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,16 @@
+puts "What is your full name?"
+full_name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+birth_year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance?"
+health_insurance = gets.chomp
+
+
