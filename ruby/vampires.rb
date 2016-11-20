9a98ab753f919dc6681f2b082bd2538889d571f2
Kristen Moore <kristen@codefu.org>
2016-11-20 17:42:50 -0600

    Update with conditional statements

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,15 +2,44 @@ puts "What is your full name?"
 full_name = gets.chomp
 
 puts "How old are you?"
-age = gets.chomp
+age_given = gets.chomp
 
 puts "What year were you born?"
 birth_year = gets.chomp
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-garlic_bread = gets.chomp
+garlic_bread_answer = gets.chomp
+
+if garlic_bread_answer.downcase == "yes"
+	likes_garlic_bread = TRUE
+else 
+  	likes_garlic_bread = FALSE
+end
+hates_garlic_bread = !likes_garlic_bread
 
 puts "Would you like to enroll in the company’s health insurance?"
-health_insurance = gets.chomp
+health_insurance_answer = gets.chomp
+
+if health_insurance_answer.downcase == "yes"
+	wants_health_insurance = TRUE
+else
+	wants_health_insurance = FALSE
+end
+waives_health_insurance = !wants_health_insurance
+
+current_year = 2016
+legit_age_true = (age_given.to_i == (current_year - birth_year.to_i))
+legit_age_false = !legit_age_true
 
 
+if legit_age_true && (likes_garlic_bread || wants_health_insurance)
+	puts "EVALUATION: Probably not a vampire."
+end
+
+if legit_age_false &&  (hates_garlic_bread || waives_health_insurance)
+	puts "EVALUATION: Probably a vampire."
+end
+
+if legit_age_false &&  (hates_garlic_bread && waives_health_insurance)
+	puts "EVALUATION: Almost certainly a vampire."
+end
