be43e3497a5840419555ad55322be42f73e303de
Kristen Moore <kristen@codefu.org>
2016-11-30 23:13:53 -0600

    Declare Puppy class and description

---
+++ ruby/puppy.rb
@@ -0,0 +1,31 @@
+# 6.1 Introduction to Classes
+
+# SPECIES ------------------------
+# Puppy
+
+# CHARACTERISTICS ----------------
+# Leg count: 4
+# Eye count: 2
+# Snout length: varies
+# Leg length: varies
+# Body length: varies
+# Tail length: varies
+# Ear style: varies
+# Fur Color: varies
+# Fluffiness: varies
+# Friendly: yes
+# Name: varies
+
+# BEHAVIOR -----------------------
+# Run
+# Jump
+# Wiggle
+# Bounce
+# Bark
+# Hug
+# Gallop
+# Fetch
+# Chew
+# Drool
+# Lick
+# Nap
