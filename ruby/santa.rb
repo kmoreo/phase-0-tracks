e63f6811905e4a8e4971c0639a27021876e2b82f
Kristen Moore <kristen@codefu.org>
2016-12-03 17:04:26 -0600

    Add getter and setter methods and test driver code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -23,6 +23,29 @@ class Santa
 		puts "Age: #{@age}"
 	end
 
+	def celebrate_birthday
+		@age = @age + 1
+	end
+
+	def get_mad_at=(reindeer_name)
+		@reindeer_ranking.delete(reindeer_name)
+		@reindeer_ranking[-1] = reindeer_name
+		puts "#{reindeer_name}, you're grounded! *angrily points to the end of the line*"
+		p @reindeer_ranking
+	end
+
+	def gender=(new_gender)
+		@gender = new_gender
+	end
+
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
 end
 
 # Driver code
@@ -39,15 +62,29 @@ example_ethnicities = ["black", "latina", "white", "jewish", "none", "armenian",
 
 cookies = ["sugar", "chocolate chip", "snickerdoodle", "ginger snap", "pfeffernusse", "biscotti", "shortbread", "rum ball"]
 
-example_genders.length.times do |i|
-	puts "New Santa Sign-up!"
-	santas << Santa.new(example_genders[i], example_ethnicities[i])
-	puts "Welcome Santa No. #{i+1}!"
-	santas[i].speak
-	santas[i].print_stats
-	santas[i].eat_milk_and_cookies(cookies.sample)
-	puts "-"*10
-end
+# example_genders.length.times do |i|
+# 	puts "New Santa Sign-up!"
+# 	santas << Santa.new(example_genders[i], example_ethnicities[i])
+# 	puts "Welcome Santa No. #{i+1}!"
+# 	santas[i].speak
+# 	santas[i].print_stats
+# 	santas[i].eat_milk_and_cookies(cookies.sample)
+# 	puts santas[i].celebrate_birthday
+# 	puts "-"*10
+# end
 
+kris = Santa.new("male", "jewish")
+
+kris.speak
+kris.eat_milk_and_cookies(cookies.sample)
+p kris.age
+p kris.ethnicity
+kris.print_stats
+kris.celebrate_birthday
+kris.get_mad_at = "Vixen"
+kris.gender = "female"
+p kris.age
+p kris.ethnicity
+kris.print_stats
 
 
