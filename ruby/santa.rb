f52d56e47f2291e6a61c326b7b2fa4cd1fb159dc
Kristen Moore <kristen@codefu.org>
2016-12-03 12:49:23 -0600

    Add cookie array and instance method calls to driver code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -5,7 +5,7 @@ class Santa
 		@gender = gender
 		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		@age = 0		
+		@age = 0
 	end
 
 	def speak
@@ -13,16 +13,41 @@ class Santa
 	end
 
 	def eat_milk_and_cookies(cookie)
-		puts "That was a good #{cookie} cookie!"
+		puts "Mmm! That was a good #{cookie} cookie!"
+	end
+
+	def print_stats
+		puts "Gender: #{@gender}"
+		puts "Ethnicity: #{@ethnicity}"
+		puts "Reindeers: #{@reindeer_ranking}"
+		puts "Age: #{@age}"
 	end
 
 end
 
 # Driver code
 
-kris = Santa.new()
-kris.speak
-kris.eat_milk_and_cookies("funfetti")
+# kris = Santa.new
+# kris.speak
+# kris.eat_milk_and_cookies("funfetti")
+
+santas = []
+
+example_genders = ["male", "female", "bigender", "transgender male", "cis female", "non-gender", "flexible", "N/A"]
+
+example_ethnicities = ["black", "latina", "white", "jewish", "none", "armenian", "Mystical Creature (unicorn)", "N/A"]
+
+cookies = ["sugar", "chocolate chip", "snickerdoodle", "ginger snap", "pfeffernusse", "biscotti", "shortbread", "rum ball"]
+
+example_genders.length.times do |i|
+	puts "New Santa Sign-up!"
+	santas << Santa.new(example_genders[i], example_ethnicities[i])
+	puts "Welcome Santa No. #{i+1}!"
+	santas[i].speak
+	santas[i].print_stats
+	santas[i].eat_milk_and_cookies(cookies.sample)
+	puts "-"*10
+end
 
 
 
