e44a134f50bab9c61a8c5e24031bd9c3df00c77c
Kristen Moore <kristen@codefu.org>
2016-12-04 01:42:28 -0600

    Create module methods and classes using them

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,45 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words.capitalize + "!!!" + " :("
+#   end
+
+#   def self.yell_happily(words)
+#     "OMG, " + words + "!!!" + " :)"
+#   end
+# end
+
+# p Shout.yell_angrily("you stink")
+
+# p Shout.yell_happily("you rock")
+
+
 module Shout
-  #methods
+	def yell_angrily(words)
+    puts words.capitalize + "!!!" + " :("
+  end
+
+  def yell_happily(words)
+    puts "Hooray, " + words + "!!!" + " :)"
+  end
 end
+
+class AnnoyingKid
+	include Shout
+end
+
+class Auctioneer
+	include Shout
+end
+
+kid = AnnoyingKid.new
+kid.yell_angrily("you stink")
+kid.yell_happily("I love unicorns")
+
+auctioneer = Auctioneer.new
+auctioneer.yell_angrily("sir, you need to leave")
+auctioneer.yell_happily("you've won the entire estate")
+
+
+
+
+	
