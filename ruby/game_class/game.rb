a851641827d9b7855b80b0e69303c27d89a5878f
Kristen Moore <kristen@codefu.org>
2016-12-04 16:56:28 -0600

    Create game class directory and subfiles

---
+++ ruby/game_class/game.rb
@@ -0,0 +1,14 @@
+# User 1 inputs a secret word.
+# Number of guesses is set to 2x word length.
+# User 2 enters a guess of a letter.
+# Program checks to see if guessed letter is in the secret word.
+# If the letter guessed has been guessed previously, the guess count does not increment.
+# If the letter guessed has not been guess previously, the guess count increments by 1.
+# If the letter is in the word, the letter is printed to the screen in its appropriate place.
+# If the letter is not in the word, the appropriate number of blank spaces is printed to the screen with no letter revealed.
+# Repeat with guesses until number of guesses is reached, or word is guessed, whichever comes first.
+# Print congratulatory/taunting message based on outcome.
+
+class WordGuess
+	#methods
+end
